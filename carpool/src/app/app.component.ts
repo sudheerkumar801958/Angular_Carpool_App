89e11647f33a2e9772faf84a6d3d0c930f2b2ecc
Sudheer Kumar Reddy KODURU <sudheerkumark.in@mouritech.com>
2024-11-04 15:51:06 +0530

    reg form api call done

--- carpool/src/app/app.component.ts
+++ carpool/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RegisterComponent } from '../components/register/register.component';
@@ -8,7 +9,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RegisterComponent,LoginComponent,],
+  imports: [RouterOutlet,RegisterComponent,LoginComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- carpool/src/components/register/register.component.ts
+++ carpool/src/components/register/register.component.ts
@@ -1,18 +1,21 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [ReactiveFormsModule,CommonModule],
+  // providers:[provideHttpClient()],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
   registrationForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,private http: HttpClient, private authService:AuthService) {
     this.registrationForm = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
@@ -26,10 +29,17 @@ export class RegisterComponent {
     const confirmPassword = form.get('confirmPassword')?.value;
     return password === confirmPassword ? null : { mismatch: true };
   }
-
   onSubmit() {
     if (this.registrationForm.valid) {
-      console.log('Form Submitted', this.registrationForm.value);
+      this.authService.register(this.registrationForm.value)
+        .subscribe(
+          response => {
+            console.log('Registration successful', response);
+          },
+          error => {
+            console.error('Registration failed', error);
+          }
+        );
     }
   }
 
