df847e260e9ff17d8a2251ef5633e910b7f13fd3
jayasree Kollaparthi <Jayasree.Kollaparthi2@sbdinc.com>
2024-11-06 15:54:23 +0530

    nav bar implemented

--- carpool/src/components/header/header.component.ts
+++ carpool/src/components/header/header.component.ts
@@ -9,11 +9,19 @@ import { User } from '../../model/user.model';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  loggedInUser: User | null = null;
-
+   userdata:any;
   constructor() {}
 
   ngOnInit(): void {
    
-  }
+      const storedUser = localStorage.getItem('user');
+      const loggedInUser = storedUser ? JSON.parse(storedUser) : null; // Parse only if storedUser is not null
+      if (loggedInUser && loggedInUser.username) {
+        this.userdata = loggedInUser.username;
+        console.log(this.userdata);
+      } else {
+        console.log("No user found in localStorage.");
+      }
+    }
+    
 }
--- carpool/src/services/user-auth.service.ts
+++ carpool/src/services/user-auth.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UserAuthService {
+  private userSubject = new BehaviorSubject<any>(null); // Initially no user
+  user$ = this.userSubject.asObservable();
  userRole='';
 
   constructor(private authService:AuthService, private router: Router) { }
@@ -18,6 +21,12 @@ export class UserAuthService {
         console.log('Login successful', response);
         localStorage.setItem('key', response.token);
         localStorage.setItem('role', response.usertype);
+        
+        const userData = {...response};
+        delete userData.token
+        console.log(userData)
+        // this.userSubject.next(userData);
+        localStorage.setItem('user', userData); 
         this.userRole = this.getUser();
         if (this.userRole === 'Admin') {
           this.router.navigate(['/offer']); // Redirect to Offer Ride page for Admins
